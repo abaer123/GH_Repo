3d96832bd951e62e35acba1ccfef6bd8c39da896
Anthony Baer <abaer@gitlab.com>
2022-12-13 02:59:50 +0000

    Upload New File

---
+++ bad_code_example.js
@@ -0,0 +1,12 @@
+function someFunc(array) {
+  var index, item, length = array.length;
+  /*
+   * Lots of code
+   */
+  for (index = 0; index < length; index++) {
+    item = array[index];
+    // Use `item`
+  }
+  return someResult;
+}
+
