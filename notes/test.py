0e008af3d50b99d743a8c37c770eee04cfbab2a8
abaer2 <abaer@gitlab.com>
2023-08-28 13:16:02 -0700

    add test.py to demo code suggestions

---
+++ notes/test.py
@@ -0,0 +1,9 @@
+""
+Generate a random number between 1 and 9 (including 1 and 9).
+Ask the user to guess the number, then tell them whether they guessed too low, too high, or exactly right.
+
+
+import random
+
+# Generate a random number between 1 and 9 (including 1 and 9).
+num = random.randint(1, 9)
