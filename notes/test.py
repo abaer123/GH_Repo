c82d87e39e5055157ae5a2d4be1d16a3dc371b89
Anthony Baer <abaer@gitlab.com>
2023-09-08 01:57:27 +0000

    continue coding

--- notes/test.py
+++ notes/test.py
@@ -6,4 +6,7 @@ Ask the user to guess the number, then tell them whether they guessed too low, t
 import random
 
 # Generate a random number between 1 and 9 (including 1 and 9).
-num = random.randint(1, 9)
+num = random.randint(1, 9)  # Generate a random number between 1 and 9 (including 1 and 9).
+
+# Ask the user to guess the number.
+guess = int(input("Guess a number between 1 and 9: "))  
